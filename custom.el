9d73cae1947725e864028c76a589be9141304f60
Walter Hilton <whilton@videologygroup.com>
2017-09-26 11:41:31 -0400

    Various tweaks/additions

--- custom.el
+++ custom.el
@@ -7,13 +7,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(compilation-window-height 25)
  '(custom-safe-themes
    (quote
     ("4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "acb039d6f2c41b3bd852b448351b2979f44ef488026c95dd5228d2f6da57f574" default)))
  '(js2-basic-offset 4)
  '(package-selected-packages
    (quote
-    (org zenburn-theme yaml-mode web-mode smex smartparens smart-mode-line slamhound rvm rspec-mode rinari rhtml-mode rainbow-delimiters projectile pretty-symbols powerline plantuml-mode paredit pallet org-plus-contrib markdown-mode magit litable leuven-theme less-css-mode json-mode js2-mode ido-yes-or-no htmlize highlight-parentheses groovy-mode graphviz-dot-mode git-gutter-fringe git fuzzy flycheck exec-path-from-shell efire discover dired+ csv-mode company command-log-mode coffee-mode clojurescript-mode cljsbuild-mode cider-decompile auto-complete))))
+    (ace-window switch-window epresent org zenburn-theme yaml-mode web-mode smex smartparens smart-mode-line slamhound rvm rspec-mode rinari rhtml-mode rainbow-delimiters projectile pretty-symbols powerline plantuml-mode paredit pallet org-plus-contrib markdown-mode magit litable leuven-theme less-css-mode json-mode js2-mode ido-yes-or-no htmlize highlight-parentheses groovy-mode graphviz-dot-mode git-gutter-fringe git fuzzy flycheck exec-path-from-shell efire discover dired+ csv-mode company command-log-mode coffee-mode clojurescript-mode cljsbuild-mode cider-decompile auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,5 +23,5 @@
  '(cursor ((t (:background "magenta1" :foreground "magenta"))))
  '(magit-diff-none ((t (:foreground "white"))))
  '(magit-item-highlight ((t (:background "grey44"))))
- '(minibuffer-prompt ((t (:foreground "brown")))))
+ '(minibuffer-prompt ((t (:foreground "orange red")))))
 ;;; custom.el ends here
